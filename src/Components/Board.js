af8083b3f31809b8eed461815ebb1544c1f1f0e9
thoover1 <thoover1@uab.edu>
2020-02-03 00:28:38 -0600

    all possible scenarious working

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -9,33 +9,57 @@ export default class Board extends Component {
     this.state = {
       board: Array(9).fill(null),
       playerX: true,
+      movesRemaining: 9,
       popup: false
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
-  checkIfWinnerDeclared(newBoard) {
+  checkIfWinnerDeclared(newBoard, updateMoves) {
     for (let i = 0; i < newBoard.length; i++) {
-      console.log(newBoard[6]);
-      // if (newBoard[0] === newBoard[1] && newBoard[1] === newBoard[2]) {
-      //   if (newBoard[0] !== null) {
-      //     window.alert("winner!");
-      //   }
-      // } else if (newBoard[0] === newBoard[3] && newBoard[3] === newBoard[6]) {
-      //   window.alert("winner 2!");
-      // } else if (newBoard[0] === newBoard[4] && newBoard[4] === newBoard[8]) {
-      //   window.alert("winner 3!");
-      // } else if (newBoard[1] === newBoard[4] && newBoard[4] === newBoard[7]) {
-      //   window.alert("winner 4!");
-      // } else if (newBoard[2] === newBoard[4] && newBoard[4] === newBoard[6]) {
-      //   window.alert("winner 5!");
-      // } else if (newBoard[2] === newBoard[5] && newBoard[5] === newBoard[8]) {
-      //   window.alert("winner 6!");
-      // } else if (newBoard[3] === newBoard[4] && newBoard[4] === newBoard[5]) {
-      //   window.alert("winner 7!");
-      // } else if (newBoard[6] === newBoard[7] && newBoard[7] === newBoard[8]) {
-      //   window.alert("winner 8!");
-      // }
+      if (newBoard[0] === newBoard[1] && newBoard[1] === newBoard[2]) {
+        if (newBoard[0] !== null) {
+          window.alert(newBoard[0] + " Wins!");
+        }
+        return;
+      } else if (newBoard[0] === newBoard[3] && newBoard[3] === newBoard[6]) {
+        if (newBoard[0] !== null) {
+          window.alert(newBoard[0] + " Wins!");
+        }
+        return;
+      } else if (newBoard[0] === newBoard[4] && newBoard[4] === newBoard[8]) {
+        if (newBoard[0] !== null) {
+          window.alert(newBoard[0] + " Wins!");
+        }
+        return;
+      } else if (newBoard[1] === newBoard[4] && newBoard[4] === newBoard[7]) {
+        if (newBoard[1] !== null) {
+          window.alert(newBoard[1] + " Wins!");
+        }
+        return;
+      } else if (newBoard[2] === newBoard[4] && newBoard[4] === newBoard[6]) {
+        if (newBoard[2] !== null) {
+          window.alert(newBoard[2] + " Wins!");
+        }
+        return;
+      } else if (newBoard[2] === newBoard[5] && newBoard[5] === newBoard[8]) {
+        if (newBoard[2] !== null) {
+          window.alert(newBoard[2] + " Wins!");
+        }
+        return;
+      } else if (newBoard[3] === newBoard[4] && newBoard[4] === newBoard[5]) {
+        if (newBoard[3] !== null) {
+          window.alert(newBoard[3] + " Wins!");
+        }
+        return;
+      } else if (newBoard[6] === newBoard[7] && newBoard[7] === newBoard[8]) {
+        if (newBoard[6] !== null) {
+          window.alert(newBoard[6] + " Wins!");
+        }
+        return;
+      } else if (updateMoves === 0) {
+        return window.alert("CAT!");
+      }
     }
   }
 
@@ -48,18 +72,19 @@ export default class Board extends Component {
   }
 
   handleClick(index, playersTurn) {
-    var newBoard = [...this.state.board];
+    let newBoard = [...this.state.board];
+    let updateMoves = this.state.movesRemaining - 1;
     newBoard.splice(index, 1, playersTurn);
     this.setState({
       board: newBoard,
-      playerX: !this.state.playerX
+      playerX: !this.state.playerX,
+      movesRemaining: updateMoves
     });
-    this.checkIfWinnerDeclared(newBoard);
+    this.checkIfWinnerDeclared(newBoard, updateMoves);
   }
 
   render() {
     const mappedBoard = this.state.board;
-
     const playersTurn = this.state.playerX ? "X" : "O";
     return (
       <div className="board-container">
